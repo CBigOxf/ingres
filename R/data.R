59d7c0e36dd3de0b55e89d35c23e6b14b49a5db2
Pedro Victori <fosforilasa@gmail.com>
2021-07-06 19:43:06 +0200

    reorganise data and write some documentation to avoid warnings

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' An example dataframe with the genes contained in the network,
+#' in the format required by the package
+"network_genes"
+
+#' An example network,
+#' in the format required by the package
+"network"
+
+#' An example small Seurat object
+"pbmc_small"
+
+#' An example dataframe with VIPER results, meant to speed up testing and demonstrations
+"viperResults"
