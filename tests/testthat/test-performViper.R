1682543e4b6f26dc79d6551193c5f2474ddd6c4f
Pedro <fosforilasa@gmail.com>
2021-09-24 20:01:23 +0200

    snapshot test only the viper object

--- tests/testthat/test-performViper.R
+++ tests/testthat/test-performViper.R
@@ -9,6 +9,6 @@ test_that("performViper returns the correct messages and output", {
   obj = createExampleIngresObject()
 
   expect_snapshot(
-    performViper(obj, regulon = aracne.networks::regulonbrca),
+    performViper(obj, regulon = aracne.networks::regulonbrca)@viper,
     cran = TRUE)
 })
