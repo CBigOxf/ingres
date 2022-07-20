a9b4179c73413985a6170908f353aedd765a733c
Pedro Victori <fosforilasa@gmail.com>
2022-07-20 17:13:14 +0100

    completed documentation and examples for performViper

--- R/performViper.R
+++ R/performViper.R
@@ -1,13 +1,23 @@
 #' Perform VIPER
 #'
 #' Performs VIPER analysis on a RNA-seq dataset contained by an ingres object.
-#' This function is merely a wrapper around the \code{\link[viper:viper]{original VIPER function}},
+#' This function is merely a wrapper around the
+#' \code{\link[viper:viper]{original VIPER function}},
 #' designed to suit the ingres workflow and properly prepare the input data.
 #' @param ingres.object An ingres object.
 #' @inheritParams viper::viper
-#' @return An ingres object with the result of the VIPER inference in the \code{viper} slot
+#' @return An ingres object with the result of the VIPER inference in
+#' the \code{viper} slot
+#'
+#' @examples
+#' ing = createIngresObjectFromSeurat(
+#'   small_blca_wang, "RNA", "data", network_genes, network
+#' )
+#'
+#' performViper(ing, aracne.networks::regulonblca)
+#'
 #' @export
-performViper = function(ingres.object, regulon, verbose = T) {
+performViper = function(ingres.object, regulon, verbose = F) {
   expression.matrix = ingres.object@expression
   # translate gene symbols to entrezid
   geneIds = suppressMessages(AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db,
@@ -15,10 +25,12 @@ performViper = function(ingres.object, regulon, verbose = T) {
     column = "ENTREZID", keytype = "SYMBOL",
     multiVals = "first"
   ))
-
-  geneIds = geneIds[which(!is.na(geneIds))] # remove those not found
-  expression.matrix = expression.matrix[names(geneIds), ] # subsetting to found genes
-  rownames(expression.matrix) = geneIds # renaming rows as entrez ids
+  # remove those not found
+  geneIds = geneIds[which(!is.na(geneIds))]
+  # subsetting to found genes
+  expression.matrix = expression.matrix[names(geneIds), ]
+  # renaming rows as entrez ids
+  rownames(expression.matrix) = geneIds
 
   # run viper on matrix using provided regulon/s
   viperMatrix = viper::viper(expression.matrix, regulon, verbose = verbose)
@@ -27,11 +39,13 @@ performViper = function(ingres.object, regulon, verbose = T) {
   viperMatrix %<>% t()
 
   # translate back to gene symbols
-  geneSymbols = suppressMessages(AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db,
-    keys = colnames(viperMatrix),
-    column = "SYMBOL", keytype = "ENTREZID",
-    multiVals = "first"
-  ))
+  geneSymbols = suppressMessages(
+    AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db,
+      keys = colnames(viperMatrix),
+      column = "SYMBOL", keytype = "ENTREZID",
+      multiVals = "first"
+    )
+  )
   colnames(viperMatrix) = geneSymbols
 
   # add cluster column to viper results
