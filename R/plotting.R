7e5d6a445802110c1554014f44aba9060f2cd3fb
Pedro Victori <fosforilasa@gmail.com>
2022-07-20 17:23:27 +0100

    completed documentation and examples for plotting

--- R/plotting.R
+++ R/plotting.R
@@ -1,12 +1,24 @@
 #' Plot a cell PBN
 #'
-#' Plots the PBN produced by \code{\link{computePbnBySingleCell}} for a given cell
-#' and returns it as a ggplot object
+#' Plots the PBN produced by \code{\link{computePbnBySingleCell}} for a
+#' given cell and returns it as a ggplot object
 #'
-#' @param ingres.object An \code{\linkS4class{ingres}} object with a non-null \code{single.cell.pbn} slot.
+#' @param ingres.object An \code{\linkS4class{ingres}} object with a non-null
+#' \code{single.cell.pbn} slot.
 #' @param cell.id A string containing the id of the cell to be plotted.
 #'
 #' @return A \code{ggplot} object containing the cell PBN plot.
+#'
+#' @examples
+#' # Create an ingres object with viper slot
+#' ing = createIngresObjectFromSeurat(
+#'   small_blca_wang, "RNA", "data", network_genes, network
+#' )
+#' ing@viper = viper_results
+#'
+#' ing = computePbnBySingleCell(ing)
+#' cellPbnPlot(ing, "sample1@ACAGCTAAGATCCCGC-1")
+#'
 #' @export
 cellPbnPlot = function(ingres.object, cell.id) {
   networkPlot(
@@ -17,13 +29,25 @@ cellPbnPlot = function(ingres.object, cell.id) {
 
 #' Plot a cluster PBN
 #'
-#' Plots the PBN produced by \code{\link{computePbnByCluster}} for a given cluster
-#' and returns it as a ggplot object
+#' Plots the PBN produced by \code{\link{computePbnByCluster}} for a given
+#' cluster and returns it as a ggplot object
 #'
-#' @param ingres.object An \code{\linkS4class{ingres}} object with a non-null \code{cluster.pbn} slot.
+#' @param ingres.object An \code{\linkS4class{ingres}} object with a non-null
+#' \code{cluster.pbn} slot.
 #' @param cluster.id A string containing the id of the cluster to be plotted.
 #'
 #' @return A \code{ggplot} object containing the cluster PBN plot.
+#'
+#' @examples
+#' # Create an ingres object with viper slot
+#' ing = createIngresObjectFromSeurat(
+#'   small_blca_wang, "RNA", "data", network_genes, network
+#' )
+#' ing@viper = viper_results
+#'
+#' ing = computePbnByCluster(ing)
+#' clusterPbnPlot(ing, "1")
+#'
 #' @export
 clusterPbnPlot = function(ingres.object, cluster.id) {
   networkPlot(
@@ -47,7 +71,10 @@ networkPlot = function(network, title) {
         ends = "last", type = "open"
       )
     ) +
-    geom_node_point(aes(fill = as.factor(fixed_function), shape = kind), size = 25) +
+    geom_node_point(aes(
+      fill = as.factor(fixed_function),
+      shape = kind
+    ), size = 25) +
     geom_node_text(aes(filter = is.na(fixed_p), label = print.id)) +
     geom_node_text(aes(filter = !is.na(fixed_p), label = print.id.p)) +
     scale_fill_manual(values = c("#DB1F48", "#01949A", "#004369")) +
@@ -61,13 +88,26 @@ networkPlot = function(network, title) {
 
 #' Plot a cluster heatmap
 #'
-#' Plots the probabilities for the identity function produced by \code{\link{computePbnByCluster}}
-#' as a heatmap, with nodes on the x axis and clusters on the y axis, and returns
-#' it as a ggplot object.
+#' Plots the probabilities for the identity function produced by
+#' \code{\link{computePbnByCluster}}
+#' as a heatmap, with nodes on the x axis and clusters on the y axis, and
+#' returns it as a ggplot object.
 #'
-#' @param ingres.object An \code{\linkS4class{ingres}} object with a non-null \code{cluster.pbn} slot.
+#' @param ingres.object An \code{\linkS4class{ingres}} object with a non-null
+#' \code{cluster.pbn} slot.
 #'
 #' @return A \code{ggplot} object containing the cluster PBN heatmap plot.
+#'
+#' @examples
+#' # Create an ingres object with viper slot
+#' ing = createIngresObjectFromSeurat(
+#'   small_blca_wang, "RNA", "data", network_genes, network
+#' )
+#' ing@viper = viper_results
+#'
+#' ing = computePbnByCluster(ing)
+#' clusterGenesHeatmap(ing)
+#'
 #' @export
 clusterGenesHeatmap = function(ingres.object) {
   p = ingres.object@cluster.pbn %>%
@@ -83,13 +123,26 @@ clusterGenesHeatmap = function(ingres.object) {
 
 #' Plot a cell heatmap
 #'
-#' Plots the probabilities for the identity function produced by \code{\link{computePbnBySingleCell}}
-#' as a heatmap, with nodes on the x axis and cells on the y axis, grouped by cluster,
-#' and returns it as a ggplot object.
+#' Plots the probabilities for the identity function produced by
+#' \code{\link{computePbnBySingleCell}} as a heatmap, with nodes on the x axis
+#'  and cells on the y axis, grouped by cluster, and returns it as a
+#'  ggplot object.
 #'
-#' @param ingres.object An \code{\linkS4class{ingres}} object with a non-null \code{single.cell.pbn} slot.
+#' @param ingres.object An \code{\linkS4class{ingres}} object with a non-null
+#' \code{single.cell.pbn} slot.
 #'
 #' @return A \code{ggplot} object containing the cell PBN heatmap plot.
+#'
+#' @examples
+#' # Create an ingres object with viper slot
+#' ing = createIngresObjectFromSeurat(
+#'   small_blca_wang, "RNA", "data", network_genes, network
+#' )
+#' ing@viper = viper_results
+#'
+#' ing = computePbnBySingleCell(ing)
+#' cellGenesHeatmap(ing)
+#'
 #' @export
 cellGenesHeatmap = function(ingres.object) {
   p = ingres.object@single.cell.pbn %>%
