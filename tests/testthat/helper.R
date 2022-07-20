75dc63ef03895af8d82d4dc6b452bda62de3a153
Pedro Victori <fosforilasa@gmail.com>
2022-07-20 14:20:30 +0100

    remove unused aracneRegulons function

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -15,22 +15,3 @@ createExampleIngresObjectAndPerformViper = function() {
 # create a object shared by all tests so it only needs to be created once,
 # except when testing the creation of the object itself.
 ingresAfterViper = createExampleIngresObjectAndPerformViper()
-
-# Load all regulons in aracne.networks
-aracneRegulons = function() {
-  c(
-    aracne.networks::regulonblca, aracne.networks::regulonbrca,
-    aracne.networks::reguloncesc, aracne.networks::reguloncoad,
-    aracne.networks::regulonesca, aracne.networks::regulongbm,
-    aracne.networks::regulonhnsc, aracne.networks::regulonkirc,
-    aracne.networks::regulonkirp, aracne.networks::regulonlaml,
-    aracne.networks::regulonlihc, aracne.networks::regulonluad,
-    aracne.networks::regulonlusc, aracne.networks::regulonnet,
-    aracne.networks::regulonov, aracne.networks::regulonpaad,
-    aracne.networks::regulonpcpg, aracne.networks::regulonprad,
-    aracne.networks::regulonread, aracne.networks::regulonsarc,
-    aracne.networks::regulonstad, aracne.networks::regulontgct,
-    aracne.networks::regulonthca, aracne.networks::regulonthym,
-    aracne.networks::regulonucec
-  )
-}
