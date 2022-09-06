46d8bdb5a0308dfe81e958b22dbf814e6c28f24c
Pedro Victori <fosforilasa@gmail.com>
2022-09-06 23:46:24 +0100

    disabling snapshot testing in ci

--- tests/testthat/test-performViper.R
+++ tests/testthat/test-performViper.R
@@ -1,4 +1,6 @@
 test_that("performViper returns the correct messages and output", {
+  skip_on_ci()
+  skip_on_cran()
   obj = createExampleIngresObject()
   expect_snapshot(
     performViper(obj, regulon = aracne.networks::regulonbrca,
