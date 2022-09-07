2e908f9435ccb0d4cfa2576378a2e3bcb122e6bc
Pedro Victori <fosforilasa@gmail.com>
2022-09-07 20:59:43 +0100

    don't install, just warn the user on bioconductor missing

--- R/setup.R
+++ R/setup.R
@@ -2,19 +2,12 @@
   biocPkgs = c("viper", "AnnotationDbi", "org.Hs.eg.db")
   for (pkg in biocPkgs) {
     if (!requireNamespace(pkg, quietly = TRUE)) {
-      if (interactive()) {
-        base::cat("Package '", pkg, "' is not available or could not be loaded. Will now try to install it from Bioconductor (requires a working internet connection):\n")
-        BiocManager::install(pkg)
-
-        # Assert that the package can be successfully loaded
-        if (!requireNamespace(pkg, quietly = TRUE)) {
-          stop(paste("Package '", pkg,
-                     "' could not be loaded. Please install it from Bioconductor, cf. https://www.bioconductor.org/"),
-               call. = FALSE)
-        }
-      } else {
-        warning("Package '", pkg, "' could not be loaded. Without it, ingres will not work. Please install it from Bioconductor, cf. https://www.bioconductor.org/")
-      }
+      stop(paste("Package '", pkg,
+                 "' could not be loaded. \n
+                     Please install it from Bioconductor\n
+                     install.packages(\"BiocManager\")\n
+                     BiocManager::install(", pkg,")"),
+           call. = FALSE)
     }
   }
 }
